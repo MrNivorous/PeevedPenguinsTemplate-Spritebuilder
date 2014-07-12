633424b112dd9367e0fb71cf560364c3ab73786a
MrNivorous <karn.sood@gmail.com>
2014-07-12 15:23:12 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,22 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Karn on 7/10/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created");
+    }
+    return self;
+}
+
+@end
